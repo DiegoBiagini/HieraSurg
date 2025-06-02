02f242e054cac7ff6b3e459d4ae3f4992dae0ed1
Biagini <biagini@lx005899.rsint.net>
2025-06-02 13:57:13 +0200

    Carousel test

--- static/js/index.js
+++ static/js/index.js
@@ -26,3 +26,27 @@ $(document).ready(function() {
     bulmaSlider.attach();
 
 })
+
+
+document
+  .querySelector('#videos-carousel')
+  .addEventListener('carousel:show', function(e){
+  
+    // e.detail.index is the zero-based index of the slide just shown
+    var idx       = e.detail.index;
+    var slides    = this.querySelectorAll('.carousel-item');
+    var active    = slides[idx];
+    var video     = active.querySelector('video');
+
+    // if there is a video, play it and when it's done, go to next slide
+    if (video){
+      // rewind / start from 0
+      video.currentTime = 0;
+      video.play();
+      
+      // when the video ends, advance the carousel
+      video.onended = function(){
+        carousel.next();
+      };
+    }
+  });
